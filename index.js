2e59167cebf72c5b790fda3644c7b39c7e31475e
Viktor Somodi <somodi.viktor@gmail.com>
2015-04-15 13:58:01 +0200

    create angular service

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var Interpolator = require('./interpolator');
+var angular = require('angular');
 var MODULE_NAME = 'sprintfInterpolator';
 
 angular
-  .module(MODULE_NAME)
+  .module(MODULE_NAME, [])
   .factory(MODULE_NAME, [function() {
     return new Interpolator();
   }]);
