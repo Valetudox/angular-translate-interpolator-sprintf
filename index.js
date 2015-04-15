a727b5180365bab504535be827d9e5d4088a89d8
Viktor Somodi <somodi.viktor@gmail.com>
2015-04-15 15:13:04 +0200

    remove angular dependency

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var Interpolator = require('./interpolator');
-var angular = require('angular');
 var MODULE_NAME = 'sprintfInterpolator';
 
-angular
-  .module(MODULE_NAME, [])
-  .factory(MODULE_NAME, [function() {
-    return new Interpolator();
-  }]);
+module.exports = function(angular) {
+  angular
+    .module(MODULE_NAME, [])
+    .factory(MODULE_NAME, [function() {
+      return new Interpolator();
+    }]);
 
-module.exports = MODULE_NAME;
+  return MODULE_NAME;
+};
