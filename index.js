b751fd777336ed264b6236ba1a2a6492ce011457
Viktor Somodi <somodi.viktor@gmail.com>
2015-04-15 13:23:02 +0200

    copy from notification center

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var vsprintf = require("sprintf-js").vsprintf;
+
+
+var SprintfInterpolator = function () {
+  this.$locale = undefined;
+};
+
+
+SprintfInterpolator.prototype = {
+
+  setLocale: function(locale) {
+    this.$locale = locale;
+  },
+
+
+  getInterpolationIdentifier: function() {
+    return 'sprintf';
+  },
+
+
+  interpolate: function(string, interpolateParams) {
+    try {
+      return vsprintf(string, interpolateParams);
+    } catch (e) {
+      return string.replace(/%s|%d/gi, '');
+    }
+  }
+
+};
+
+module.exports = SprintfInterpolator;
